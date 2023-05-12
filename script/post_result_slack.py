b19cf7cd50ad4a4a8de0e01bcc27a3ea62d63e94
Muhammed Salman <mahmadslman@gmail.com>
2023-05-12 15:36:07 +0300

    test

--- script/post_result_slack.py
+++ script/post_result_slack.py
@@ -16,39 +16,40 @@ else:
 # the input file is a json file in the format of [ { "axis_value": "Pixel3-30-en-portrait", "outcome": "Passed", "test_details": "--" } ]
 # read the input file
 with open(input_file) as f:
-    data = json.load(f)
-    # format the json to be like
-    # <b>Test Details</b>
-    # Device: Pixel3-30-en-portrait - <green>Passed</green>
-    # Device: Pixel3-30-en-portrait - <red>Failed</red>
-    # Device: Pixel3-30-en-portrait - <yellow>Skipped</yellow>
-    
-    message = ""
-    if app_version != "":
-        message += "> *Version*\n"
-        message += "> " + app_version + "\n"
+    try:
+        print("Reading input file...")
+        print("lines: " + str((f.readlines())))
+        data = json.load(f)
+        
+        message = ""
+        if app_version != "":
+            message += "> *Version*\n"
+            message += "> " + app_version + "\n"
 
-    message += "> *Test Details*\n"
-    for item in data:
-        name = item["axis_value"]
-        outcome = item["outcome"]
-        if outcome == "Passed":
-            mark = ":white_check_mark:"
-        elif outcome == "Failed":
-            mark = ":x:"
+        message += "> *Test Details*\n"
+        for item in data:
+            name = item["axis_value"]
+            outcome = item["outcome"]
+            if outcome == "Passed":
+                mark = ":white_check_mark:"
+            elif outcome == "Failed":
+                mark = ":x:"
+            else:
+                mark = ":warning:"
+            message += "> Device: " + name + " - " +  outcome + " " + mark + "\n"
+
+        payload = {
+            "text": message
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        
+        response = requests.post(slack_webhook_url, data=json.dumps(payload), headers=headers)
+        if response.status_code != 200:
+            print(f"Error sending message to Slack: {response.text}")
         else:
-            mark = ":warning:"
-        message += "> Device: " + name + " - " +  outcome + " " + mark + "\n"
-
-    payload = {
-        "text": message
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    
-    response = requests.post(slack_webhook_url, data=json.dumps(payload), headers=headers)
-    if response.status_code != 200:
-        print(f"Error sending message to Slack: {response.text}")
-    else:
-        print("Message sent successfully!")
+            print("Message sent successfully!")
+    except Exception as e:
+        print("Error reading input file or sending message to Slack: " + str(e))
+        
