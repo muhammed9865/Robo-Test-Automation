7ab41848b97f3f67e6a2c43c7ea1588ebbbf6cf6
Muhammed Salman <mahmadslman@gmail.com>
2023-05-12 15:15:04 +0300

    test

--- script/post_result_slack.py
+++ script/post_result_slack.py
@@ -1,46 +1,54 @@
-# write a script to get the input file and clean the result file and post the result to slack
-
-import os
-import sys
-import json
 import requests
+import json
+import sys
+import os
+
 
 # get the input file
 input_file = sys.argv[1]
-
-# the input file is the result file, so we need to clean it
-# read the file
-with open(input_file, 'r') as f:
-    lines = f.readlines()
-    firstLetterToCheck = '┌'
-    latestLetterToCheck = '┘'
-    # get all the text between the firstLetterToCheck and latestLetterToCheck
-    # and write it to a new file
-    new_lines = []
-    for line in lines:
-        if firstLetterToCheck in line:
-            start = lines.index(line)
-        if latestLetterToCheck in line:
-            end = lines.index(line)
-        
-    new_lines = lines[start:end+1]
-    # write the new lines to a new file
-    with open('cleaned_result.txt', 'w') as f:
-        for line in new_lines:
-            f.write(line)
-    
-# post the result to slack
-# get the slack webhook url from the args
+# get the slack webhook url
 slack_webhook_url = sys.argv[2]
-# read the cleaned result file
-with open('cleaned_result.txt', 'r') as f:
-    lines = f.readlines()
-    # get the text from the file
-    text = ''
-    for line in lines:
-        text += line
-    # post the text to slack
+if len(sys.argv) > 3:
+    app_version = sys.argv[3]
+else:
+    app_version = ""
+
+# the input file is a json file in the format of [ { "axis_value": "Pixel3-30-en-portrait", "outcome": "Passed", "test_details": "--" } ]
+# read the input file
+with open(input_file) as f:
+    data = json.load(f)
+    # format the json to be like
+    # <b>Test Details</b>
+    # Device: Pixel3-30-en-portrait - <green>Passed</green>
+    # Device: Pixel3-30-en-portrait - <red>Failed</red>
+    # Device: Pixel3-30-en-portrait - <yellow>Skipped</yellow>
+    
+    message = ""
+    if app_version != "":
+        message += "> *Version*\n"
+        message += "> " + app_version + "\n"
+
+    message += "> *Test Details*\n"
+    for item in data:
+        name = item["axis_value"]
+        outcome = item["outcome"]
+        if outcome == "Passed":
+            mark = ":white_check_mark:"
+        elif outcome == "Failed":
+            mark = ":x:"
+        else:
+            mark = ":warning:"
+        message += "> Device: " + name + " - " +  outcome + " " + mark + "\n"
+
     payload = {
-        'text': text
+        "text": message
     }
-    requests.post(slack_webhook_url, json.dumps(payload))
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    response = requests.post(slack_webhook_url, data=json.dumps(payload), headers=headers)
+    if response.status_code != 200:
+        print(f"Error sending message to Slack: {response.text}")
+    else:
+        print("Message sent successfully!")
